4f5d5c87b133c14799cff846fbbd1b0feb46b72b
DalatCoder <hieuntctk42@gmail.com>
2020-08-22 08:47:17 +0700

    Delete a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -221,4 +221,50 @@ router.post(
     }
   }
 );
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete a comment on a post
+// @access  Private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) {
+      return res.json(404).json({ msg: 'Post not found' });
+    }
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (cmt) => cmt.id.toString() === req.params.comment_id
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exists' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map((cmt) => cmt.id.toString())
+      .indexOf(req.params.comment_id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Comment does not exists' });
+    }
+
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
