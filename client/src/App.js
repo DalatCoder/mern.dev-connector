34f755536c4d0c12d944a9d13591420ca0482d32
DalatCoder <hieuntctk42@gmail.com>
2020-08-23 09:25:14 +0700

    Protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 
+import { PrivateRoute } from './components/routing/PrivateRoute';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import { Alert } from './components/layout/Alert';
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
+import { Dashboard } from './components/dashboard/Dashboard';
 import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
@@ -33,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const _PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      !isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...rest} />
+      )
+    }
+  />
+);
+
+_PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export const PrivateRoute = connect(mapStateToProps)(_PrivateRoute);
