656f7614f881903474b6c940cca56b974c58268d
DalatCoder <hieuntctk42@gmail.com>
2020-08-23 22:12:17 +0700

    Add ProfileGithub component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import { ProfileTop } from './ProfileTop.js';
 import { ProfileAbout } from './ProfileAbout';
 import { ProfileExperience } from './ProfileExperience';
 import { ProfileEducation } from './ProfileEducation';
+import { ProfileGithub } from './ProfileGithub';
 import { Spinner } from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 
@@ -64,6 +65,10 @@ const _Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
