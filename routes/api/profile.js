105e685b9d22bd0596672b78a8396667e00a9abe
DalatCoder <hieuntctk42@gmail.com>
2020-08-23 17:25:03 +0700

    Delete all posts by Uesr

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const auth = require('../../middlewares/auth');
 
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user profile
@@ -150,6 +151,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
 
