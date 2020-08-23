536415f17f3acc2866021f5dc3201a71bcd183af
DalatCoder <hieuntctk42@gmail.com>
2020-08-23 15:03:22 +0700

    Edit profile page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 import { Dashboard } from './components/dashboard/Dashboard';
 import { CreateProfile } from './components/profile-forms/CreateProfile';
+import { EditProfile } from './components/profile-forms/EditProfile';
 import setAuthToken from './utils/setAuthToken';
 
 if (localStorage.token) {
@@ -41,6 +42,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -1,10 +1,15 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { createProfile } from '../../actions/profile';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
-const _CreateProfile = ({ createProfile, history }) => {
+const _EditProfile = ({
+  createProfile,
+  getCurrentProfile,
+  profile: { profile, loading },
+  history,
+}) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -20,6 +25,25 @@ const _CreateProfile = ({ createProfile, history }) => {
     instagram: '',
   });
   const [displaySocialInputs, setDisplaySocialInputs] = useState(false);
+
+  useEffect(() => {
+    getCurrentProfile();
+    setFormData({
+      company: loading || !profile.company ? '' : profile.company,
+      website: loading || !profile.website ? '' : profile.website,
+      location: loading || !profile.location ? '' : profile.location,
+      status: loading || !profile.status ? '' : profile.status,
+      skills: loading || !profile.skills ? '' : profile.skills.toString(),
+      githubusername:
+        loading || !profile.githubusername ? '' : profile.githubusername,
+      bio: loading || !profile.bio ? '' : profile.bio,
+      twitter: loading || !profile.social ? '' : profile.social.twitter,
+      facebook: loading || !profile.social ? '' : profile.social.facebook,
+      linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+      youtube: loading || !profile.social ? '' : profile.social.youtube,
+      instagram: loading || !profile.social ? '' : profile.social.instagram,
+    });
+  }, []);
 
   const {
     company,
@@ -41,7 +65,7 @@ const _CreateProfile = ({ createProfile, history }) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    createProfile(formData, history);
+    createProfile(formData, history, true);
   };
 
   return (
@@ -214,10 +238,17 @@ const _CreateProfile = ({ createProfile, history }) => {
   );
 };
 
-_CreateProfile.propTypes = {
+_EditProfile.propTypes = {
   createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
-export const CreateProfile = connect(null, { createProfile })(
-  withRouter(_CreateProfile)
-);
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export const EditProfile = connect(mapStateToProps, {
+  createProfile,
+  getCurrentProfile,
+})(withRouter(_EditProfile));
